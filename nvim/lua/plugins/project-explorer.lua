39b9b8241e4abd36363f457827a5fbbf48910a54
racimfethallah <ricdev.io@gmail.com>
2024-07-30 23:51:19 +0100

    updated neovim

--- nvim/lua/plugins/project-explorer.lua
+++ nvim/lua/plugins/project-explorer.lua
@@ -2,10 +2,17 @@ return {
   "Rics-Dev/project-explorer.nvim",
   dependencies = {
     "nvim-telescope/telescope.nvim",
-    "prichrd/netrw.nvim",
   },
   opts = {
-    paths = { "~/dev/*" },
+    paths = { "~/dev/*" }, --custom path set by user
+    file_explorer = function(dir) --custom file explorer set by user
+      vim.cmd("Neotree close")
+      vim.cmd("Neotree " .. dir)
+    end,
+    -- Or for oil.nvim:
+    -- file_explorer = function(dir)
+    --   require("oil").open(dir)
+    -- end,
   },
   config = function(_, opts)
     require("project_explorer").setup(opts)
