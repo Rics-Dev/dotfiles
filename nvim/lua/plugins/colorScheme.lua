5a557195da7759ef1bc8417c5086c51b16e5af4d
Ric's Dev <ricsdev@proton.me>
2026-02-13 23:16:17 +0100

    updated dotfiles

--- nvim/lua/plugins/colorScheme.lua
+++ nvim/lua/plugins/colorScheme.lua
@@ -1,50 +1,57 @@
 return {
-  -- flow theme
-  {
-    "Rics-Dev/flow.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("flow").setup({
-        transparent_background = false,
-      })
-      vim.cmd("colorscheme flow")
-    end,
-  },
-  -- {
-  -- "olimorris/onedarkpro.nvim",
-  -- lazy = false,
-  -- priority = 1000,
-  -- config = function()
-  --   require("onedarkpro").setup({
-  --   })
-  --   vim.cmd("colorscheme onedark")
-  -- end,
-  --  },
-  -- {
-  --   "marko-cerovac/material.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("material").setup({
-  --     })
-  --       vim.cmd("colorscheme material-deep-ocean")
-  --   end,
-  -- },
+	-- { "ellisonleao/gruvbox.nvim" },
+	-- {
+	-- 	"LazyVim/LazyVim",
+	-- 	opts = {
+	-- 		colorscheme = "gruvbox",
+	-- 	},
+	-- },
+	-- flow theme
+	{
+	  "Rics-Dev/flow.nvim",
+	  lazy = false,
+	  priority = 1000,
+	  config = function()
+	    require("flow").setup({
+	      transparent_background = false,
+	    })
+	    vim.cmd("colorscheme flow")
+	  end,
+	},
+	-- {
+	-- "olimorris/onedarkpro.nvim",
+	-- lazy = false,
+	-- priority = 1000,
+	-- config = function()
+	--   require("onedarkpro").setup({
+	--   })
+	--   vim.cmd("colorscheme onedark")
+	-- end,
+	--  },
+	-- {
+	--   "marko-cerovac/material.nvim",
+	--   lazy = false,
+	--   priority = 1000,
+	--   config = function()
+	--     require("material").setup({
+	--     })
+	--       vim.cmd("colorscheme material-deep-ocean")
+	--   end,
+	-- },
 
-  -- {
-  --   "metalelf0/black-metal-theme-neovim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("black-metal").setup({
-  --       theme = "thyrfing",
-  --       -- theme = "nile",
-  --       -- theme = "khold",
-  --       -- theme = "darkthrone",
-  --       -- theme = "venom",
-  --     })
-  --     require("black-metal").load()
-  --   end,
-  -- }
+	-- {
+	--   "metalelf0/black-metal-theme-neovim",
+	--   lazy = false,
+	--   priority = 1000,
+	--   config = function()
+	--     require("black-metal").setup({
+	--       theme = "thyrfing",
+	--       -- theme = "nile",
+	--       -- theme = "khold",
+	--       -- theme = "darkthrone",
+	--       -- theme = "venom",
+	--     })
+	--     require("black-metal").load()
+	--   end,
+	-- }
 }
